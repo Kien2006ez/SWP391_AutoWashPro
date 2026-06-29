37b8aeea4af523f7d129888a39502ee1489af33c
bodvn2 <dungtt6715@ut.edu.vn>
2026-06-30 01:22:52 +0700

    chỉnh sửa cấu trúc dự án sửa file requirement.txt cho phù hợp

--- src/routes/service.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-service_bp = Blueprint('service', __name__)
--- src/static/js/app.js
+++
@@ -1,5 +0,0 @@
-// AutoWashPro frontend helper script
-
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("AutoWashPro static JS loaded.");
-});
