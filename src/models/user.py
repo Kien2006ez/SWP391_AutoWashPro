ba52e3538234cbb275188926d05f7a34b51a00e0
Quynh Nhung <nhungltq8095@ut.edu.vn>
2026-07-02 09:46:02 +0700

    xóa file lỗi

--- src/models/user.py
+++
@@ -1,6 +0,0 @@
-from src.database.db import db
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
--- src/models/vehicle.py
+++
@@ -1,6 +0,0 @@
-from src.database.db import db
-
-class Vehicle(db.Model):
-    __tablename__ = 'vehicles'
-    id = db.Column(db.Integer, primary_key=True)
-    license_plate = db.Column(db.String(20), nullable=False)
